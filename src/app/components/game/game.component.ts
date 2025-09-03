0096a990c5fc934c759449e2e140c24b8cd39666
XavierCortes98 <86173666+XavierCortes98@users.noreply.github.com>
2025-09-03 18:35:12 +0200

    main poster and content done

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
-
+import { MovieInfo } from '../../models/movieInfo.model';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-game',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './game.component.html',
-  styleUrl: './game.component.scss'
+  styleUrl: './game.component.scss',
 })
 export class GameComponent {
+  isLoading = false;
+  showResult = false;
+  userGuess: number = 0;
 
+  currentMovie: MovieInfo | null = {
+    id: 755898,
+    poster_path: '/yvirUYrva23IudARHn3mMGVxWqM.jpg',
+    release_date: '2025-07-29',
+    title: 'War of the Worlds',
+    vote_average: 4.319,
+  };
 }
---
+++ src/app/models/movieInfo.model.ts
@@ -0,0 +1,7 @@
+export interface MovieInfo {
+  id: number;
+  title: string;
+  release_date: string;
+  vote_average: number;
+  poster_path: string;
+}
